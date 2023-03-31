5a8eec4ef13f29875c81a1eb51b8f53b536591cf
dwwe2017 <daniel@dwwe.de>
2023-03-31 07:31:22 +0200

    - Es ist nun möglich Zugangsdaten für die Nutzung eines Powerfox Poweropti zu hinterlegen um die Genauigkeit der Daten zu verbessern

--- enpal.sh
+++ enpal.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-# Zugangsdaten
+# Zugangsdaten InfluxDB
+# @ see https://github.com/weldan84/enpal-influx-evcc
 INFLUX_HOST="YOUR_INFLUX_HOST"
 INFLUX_ORG_ID="YOUR_INFLUX_ORG_ID"
 INFLUX_BUCKET="YOUR_INFLUX_BUCKET"
 INFLUX_TOKEN="YOUR_INFLUX_TOKEN"
 INFLUX_API="${INFLUX_HOST}/api/v2/query?orgID=${INFLUX_ORG_ID}"
 QUERY_RANGE_START="-5m"
+
+# Zugangsdaten Powerfox Poweropti
+# @see https://www.powerfox.energy/daten-von-powerfox-per-api-nutzen/
+POWERFOX_USERNAME="POWERFOX_USERNAME"
+POWERFOX_PASSWORD="POWERFOX_PASSWORD"
+POWERFOX_DEVICE_ID="POWERFOX_DEVICE_ID"
 
 case $1 in
 # Gesamtverbrauch
@@ -24,14 +31,29 @@ consumption)
   status="$?"
   var="${var##*,}"
   ;;
-# Netzbezug/Einspeisung
-grid)
+# Netzbezug/Einspeisung Enpal InfluxDB (Errechneter Wert)
+grid_enpal)
   pv=$(enpal pv)
   consumption=$(enpal consumption)
   battery=$(enpal battery)
   # shellcheck disable=SC2004
   echo $(($consumption - $pv - $battery))
   exit 0
+  ;;
+# Netzbezug/Einspeisung Powerfox Poweropti (Tatsächlicher Wert)
+grid_powerfox)
+  # Um das API-Aufrufkontingent nicht zu überschreiten wird hier 3 Sekunden lang pausiert! (Maximal zugelassen 1 pro 3s)
+  sleep 3
+  var=$(curl -f -s -G "https://backend.powerfox.energy/api/2.0/my/$POWERFOX_DEVICE_ID/current" \
+    -u "$POWERFOX_USERNAME:$POWERFOX_PASSWORD")
+  status="$?"
+  var=$(echo "$var" | jq '.Watt // empty')
+  if [ "$var" = "empty" ]; then
+    echo >&2 "Der Poweropti liefert keine aktuellen Werte. Hier wird dir geholfen https://poweropti.powerfox.energy/faq/"
+    exit 1
+  fi
+  echo "$var"
+  exit "$status"
   ;;
 # Aktuelle Solarproduktion / DC-Erzeugungsleistung
 pv)
@@ -41,8 +63,8 @@ pv)
     --header "Content-type: application/vnd.flux" \
     --data "from(bucket: \"$INFLUX_BUCKET\")
             |> range(start: $QUERY_RANGE_START)
-            |> filter(fn: (r) => r._measurement == \"Gesamtleistung\")
-            |> filter(fn: (r) => r._field == \"Produktion\")
+            |> filter(fn: (r) => r._measurement == \"LeistungDc\")
+            |> filter(fn: (r) => r._field == \"Total\")
             |> keep(columns: [\"_value\"])
             |> last()")
   status="$?"
@@ -131,23 +153,37 @@ ac)
   var="${var##*,}"
   ;;
 # Batterieleistung
-# Die maximale Ladeleistung ist auf 5000W begrenzt, die maximale Entladeleistung auf -5000W. Dieser Wert kann/muss je nach Speicher angepasst werden.
+# Die maximale Entladeleistung ist auf 5000W begrenzt, die maximale Ladeleistung auf -5000W. Dieser Wert kann/muss je nach Speicher angepasst werden.
 battery)
   pv=$(enpal pv)
   ac=$(enpal ac)
-  # shellcheck disable=SC2004
-  battery=$(($ac - $pv))
-  if [ "$ac" -lt "$pv" ]; then
-    echo 0
-    exit 0
-  elif [ $battery -lt -5000 ]; then
-    echo -5000
-    exit 0
-  elif [ $battery -gt 5000 ]; then
-    echo 5000
-    exit 0
+  if [ "$POWERFOX_USERNAME" = "POWERFOX_USERNAME" ] || [ "$POWERFOX_PASSWORD" = "POWERFOX_PASSWORD" ]; then
+    # Falls Powerfox Poweropti nicht genutzt wird, muss der Ladezustand geschätzt werden
+    if [ "$ac" -lt "$pv" ]; then
+      echo 0
+      exit 0
+    else
+      battery=$(($ac - $pv))
+      if [ "$battery" -lt -5000 ]; then
+        echo -5000
+        exit 0
+      elif [ "$battery" -gt 5000 ]; then
+        echo 5000
+        exit 0
+      fi
+    fi
+  else
+    # Falls Powerfox Poweropti vorhanden ist, kann mit Sicherheit bestimmt werden wie hoch die
+    # Batterieleistung ist, indem die Werte für den tatsächlichen Netzbezug mit einberechnet werden.
+    grid=$(enpal grid_powerfox)
+    if [ "$grid" -gt 0 ] && [ "$ac" -lt "$pv" ]; then
+      echo 0
+    else
+      battery=$(($ac - $pv + $grid))
+    fi
   fi
-  echo $battery
+
+  echo "$battery"
   exit 0
   ;;
 # Ladezustand der Batterie (bisher nur 0%, 50% oder 100% möglich)
@@ -169,7 +205,8 @@ soc)
 # Kleine Gedankenstütze für die Konsole ;)
 help)
   echo consumption
-  echo grid
+  echo grid_enpal
+  echo grid_powerfox
   echo pv
   echo energy
   echo phase \[1-3\]
